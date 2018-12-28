e7fa151c1482de605450108fec33a8424b5aa24f
Gry0u <gauthriou@protonmail.com>
2018-12-28 15:26:31 +0100

    feat: Reduce validation window when resubmitting a request

--- MemPool.js
+++ MemPool.js
@@ -12,8 +12,19 @@ class MemPool {
 
   addValidationRequest (request) {
     const TimeoutRequestsWindowTime = 5 * 60 * 1000
-    this.mempool.push(request)
-    setTimeout(_ => { this.removeValidationRequest(request.walletAddress) }, TimeoutRequestsWindowTime)
+    // if request already in mempool, reduce validation window
+    if (this.mempool.filter(valRequest => {
+      return valRequest.walletAddress === request.walletAddress
+    }).length) {
+      const timeElapse = (new Date().getTime().toString().slice(0, -3)) - request.requestTimeStamp
+      const timeLeft = (TimeoutRequestsWindowTime / 1000) - timeElapse
+      request.validationWindow = timeLeft
+    } else {
+      // Otherwise just add request to mempool
+      this.mempool.push(request)
+      // Remove automatically request from mempool after 5 minutes
+      setTimeout(_ => { this.removeValidationRequest(request.walletAddress) }, TimeoutRequestsWindowTime)
+    }
   }
 }
 module.exports.MemPool = MemPool
