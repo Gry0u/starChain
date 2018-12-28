6ef5d854dc6b1a42b26e4c9bd1ae27dbf4355314
Gry0u <gauthriou@protonmail.com>
2018-12-28 14:54:43 +0100

    feat: Set validation window of 5 minutes

--- MemPool.js
+++ MemPool.js
@@ -1,11 +1,27 @@
 class MemPool {
   constructor () {
     this.mempool = []
-    this.timeoutRequests = []
+    // this.timeoutRequests = {}
+  }
+
+  removeValidationRequest (walletAddress) {
+    this.mempool = this.mempool.filter(valRequest => {
+      return valRequest.walletAddress !== walletAddress
+    })
   }
 
   addValidationRequest (request) {
+    const TimeoutRequestsWindowTime = 5 * 60 * 1000
     this.mempool.push(request)
+    setTimeout(_ => { this.removeValidationRequest(request.walletAddress) }, TimeoutRequestsWindowTime)
   }
 }
 module.exports.MemPool = MemPool
+
+// self.timeoutRequests[request.walletAddress]=setTimeout(function(){ self.removeValidationRequest(request.walletAddress) }, TimeoutRequestsWindowTime );
+//
+// const TimeoutRequestsWindowTime = 5*60*1000;
+//
+// let timeElapse = (new Date().getTime().toString().slice(0,-3)) - req.requestTimeStamp;
+// let timeLeft = (TimeoutRequestsWindowTime/1000) - timeElapse;
+// req.validationWindow = timeLeft;
--- Request.js
+++ Request.js
@@ -1,9 +1,9 @@
 class Request {
-  constructor (walletAdress) {
-    this.walletAdress = walletAdress
+  constructor (walletAddress) {
+    this.walletAddress = walletAddress
     this.requestTimeStamp = new Date().getTime().toString().slice(0, -3)
     this.validationWindow = 300
-    this.message = `${this.walletAdress}:${this.requestTimeStamp}:starRegistry`
+    this.message = `${this.walletAddress}:${this.requestTimeStamp}:starRegistry`
   }
 }
 
