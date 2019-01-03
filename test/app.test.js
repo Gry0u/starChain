0f5ed3c4e5bfecb3473935520d3ec4cc941f55d5
Gry0u <gauthriou@protonmail.com>
2019-01-03 19:03:41 +0100

    test: Finish test second POST endpoint

--- test/app.test.js
+++ test/app.test.js
@@ -10,38 +10,37 @@ const app = require('../app')
 const keyPair = bitcoin.ECPair.makeRandom()
 const privateKey = keyPair.privateKey
 const { address } = bitcoin.payments.p2pkh({ pubkey: keyPair.publicKey })
-fs.writeFileSync('data/address.txt', address)
 
-test.cb('1. /requestValidation: returns a Request JSON object', (t) => {
-  supertest(SERVER_URL)
-    .post('/requestValidation')
-    .send({ address: address })
-    .expect(200)
-    .expect((response) => {
-      t.is(response.status, 200)
-      t.is(response.body.address, address)
-      t.is(response.body.validationWindow, 300)
-      t.hasOwnProperty('requestTimeStamp')
-      t.is(response.body.message, `${address}:${response.body.requestTimeStamp}:starRegistry`)
+const options1 = {
+  method: 'POST',
+  uri: SERVER_URL + '/requestValidation',
+  body: {
+    address: address
+  },
+  json: true
+}
 
-      // write signature into text file for reuse in further tests
-      const signature = bitcoinMessage.sign(response.body.message, privateKey, keyPair.compressed).toString('base64')
-      fs.writeFileSync('data/signature.txt', signature)
-    })
-    .end(t.end)
+test('1. /requestValidation: returns a Request JSON object', async t => {
+  const response = await rp(options1)
+  t.is(response.address, address)
+  t.is(response.validationWindow, 300)
+  t.is(response.message, `${address}:${response.requestTimeStamp}:starRegistry`)
 })
 
 test('2. /message-signature/validate: returns a JSON object with registerStar and status properties', async t => {
-  const signature = await fs.readFileAsync('data/signature.txt')
-  const options = {
+  // const signature = await fs.readFileAsync('data/signature.txt')
+
+  const message = (await rp(options1)).message
+  const signature = bitcoinMessage.sign(message, privateKey, keyPair.compressed).toString('base64')
+  const options2 = {
     method: 'POST',
     uri: SERVER_URL + '/message-signature/validate',
     body: {
       address: address,
-      signature: signature.toString()
+      signature: signature
     },
     json: true
   }
-  const response = await rp(options)
-  t.true(response.registerStar)
+  const response2 = await rp(options2)
+  t.true(response2.registerStar)
 })
