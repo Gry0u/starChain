6a7c67638df524d3e5a1d973a74fe34f5b4c22c5
Gauthier Riou <gauthier.riou@airbus.com>
2019-01-04 10:10:18 +0100

    test: Async tests with request-promise

--- test/app.test.js
+++ test/app.test.js
@@ -1,8 +1,5 @@
 const test = require('ava')
-const bluebird = require('bluebird')
 const rp = require('request-promise')
-const fs = bluebird.promisifyAll(require('fs'))
-const supertest = require('supertest')
 const bitcoin = require('bitcoinjs-lib')
 const bitcoinMessage = require('bitcoinjs-message')
 const SERVER_URL = 'http://localhost:8000'
@@ -10,38 +7,34 @@ const app = require('../app')
 const keyPair = bitcoin.ECPair.makeRandom()
 const privateKey = keyPair.privateKey
 const { address } = bitcoin.payments.p2pkh({ pubkey: keyPair.publicKey })
-fs.writeFileSync('data/address.txt', address)
+const optionsRequestValidation = {
+  method: 'POST',
+  uri: SERVER_URL + '/requestValidation',
+  body: { address: address },
+  json: true
+}
 
-test.cb('1. /requestValidation: returns a Request JSON object', (t) => {
-  supertest(SERVER_URL)
-    .post('/requestValidation')
-    .send({ address: address })
-    .expect(200)
-    .expect((response) => {
-      t.is(response.status, 200)
-      t.is(response.body.address, address)
-      t.is(response.body.validationWindow, 300)
-      t.hasOwnProperty('requestTimeStamp')
-      t.is(response.body.message, `${address}:${response.body.requestTimeStamp}:starRegistry`)
-
-      // write signature into text file for reuse in further tests
-      const signature = bitcoinMessage.sign(response.body.message, privateKey, keyPair.compressed).toString('base64')
-      fs.writeFileSync('data/signature.txt', signature)
-    })
-    .end(t.end)
+test.serial('1. /requestValidation: returns a Request JSON object', async t => {
+  const requestObject = await rp(optionsRequestValidation)
+  t.is(requestObject.address, address)
+  t.is(requestObject.validationWindow, 300)
+  t.is(requestObject.message, `${address}:${requestObject.requestTimeStamp}:starRegistry`)
 })
 
-test('2. /message-signature/validate: returns a JSON object with registerStar and status properties', async t => {
-  const signature = await fs.readFileAsync('data/signature.txt')
-  const options = {
+test.serial('2. /message-signature/validate: returns a JSON object with registerStar and status.messageSignature properties true', async t => {
+  const message = (await rp(optionsRequestValidation)).message
+  const signature = bitcoinMessage.sign(message, privateKey, keyPair.compressed).toString('base64')
+  const optionsMessageSignature = {
     method: 'POST',
     uri: SERVER_URL + '/message-signature/validate',
     body: {
       address: address,
-      signature: signature.toString()
+      signature: signature
     },
     json: true
   }
-  const response = await rp(options)
-  t.true(response.registerStar)
+  const validRequestObj = await rp(optionsMessageSignature)
+  t.true(validRequestObj.registerStar)
+  t.true(validRequestObj.status.messageSignature)
+  t.is(validRequestObj.status.address, address)
 })
